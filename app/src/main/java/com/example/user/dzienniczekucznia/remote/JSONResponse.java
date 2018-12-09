2516f5bde324dbcdb0c35a58a6626bd8a693f147
m.prekiel <m.prekiel@csioz.gov.pl>
2018-12-09 20:22:12 +0100

    Dodanie obsługi odpowiedzi JSON

--- app/src/main/java/com/example/user/dzienniczekucznia/remote/JSONResponse.java
+++ app/src/main/java/com/example/user/dzienniczekucznia/remote/JSONResponse.java
@@ -2,11 +2,51 @@ package com.example.user.dzienniczekucznia.remote;
 
 
 import com.example.user.dzienniczekucznia.models.Grupy;
+import com.example.user.dzienniczekucznia.models.User;
+
+import java.util.List;
 
 public class JSONResponse {
+
+    private Long id;
+
+    private String name;
+
+    private List <String> Users;
+
+    public List<String> getUsers() {
+        return Users;
+    }
+    public void setUsers(List<String> users){
+        this.Users = Users;
+    }
+
+
+
     private Grupy[] grupy;
 
     public Grupy[] getGrupy() {
         return grupy;
     }
+
+    public Long getId() {
+        return id;
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
 }
